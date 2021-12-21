88b751d6168c8e57b2f7e26a65efa6181b257d9f
Peter <migalega.p@gmail.com>
2021-12-21 06:34:20 +0200

    changed port again

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def detect():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
